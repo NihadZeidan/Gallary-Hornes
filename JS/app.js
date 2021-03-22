b896a2afd758bac3bc64492ed93e09e61a0c7afd
NihadZeidan <nihadzeidan@gmail.com>
2021-03-22 20:43:02 +0200

    first try

---
+++ JS/app.js
@@ -0,0 +1,80 @@
+'use strict';
+$('document').ready(function () {
+    
+
+function Horn(horn) {
+    this.title = horn.title;
+    this.description = horn.description;
+    this.image = horn.image_url;
+    this.keyword = horn.keyword;
+}
+
+
+Horn.prototype.renderFromClone = function () {
+    let template = $('#photo-template').clone();
+    template.find("h2").text(this.title);
+    template.find('p').text(this.description);
+    template.find('img').attr("src", this.image);
+    template.removeAttr('id')
+    $('main').append(template);
+
+}
+
+
+Horn.prototype.addToSelect = function () {
+    $('select').append(
+        `<option value="${this.keyword}"> ${this.keyword} </option>`
+    )
+
+    // $('option').on("click", function () {
+    //     $("div").remove();
+    // })
+
+
+}
+
+
+
+
+
+let ajaxSettings = {
+    method: 'get',
+    dataType: 'json'
+}
+
+
+
+$.ajax('data/page-1.json', ajaxSettings).then((data) => {
+    data.forEach(function (horn, index) {
+        let newHorn = new Horn(horn);
+        newHorn.renderFromClone();
+
+
+
+
+
+        if (horn.keyword != $("option").attr('value')) {
+            newHorn.addToSelect();
+            console.log($("option").attr('value'));
+        } else {
+            $('option').hide();
+        }
+
+        // console.log(keyWord);
+
+    })
+
+
+
+    // This to remove the defult template after render all the inctances:
+    $('#photo-template').remove()
+
+
+    // let keyWord = [];
+
+
+})
+
+
+})
+
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# lab-02-code301
+### lab-02 
+
+Number and name of feature:  Feature #1: Display images
+
+Estimate of time needed to complete: 1 H
+
+Start time: 2:30
+
+Finish time: 3:15 
+
+Actual time needed to complete: 45 Min
+
+----------------
+
+
+Number and name of feature: Feature #2: Filter images
+
+Estimate of time needed to complete: 1.5 H
+
+Start time: 3:20
+
+Finish time: 
+
+Actual time needed to complete: 
+
+------------
+
+
+Number and name of feature: Feature #3: Style the application
+
+Estimate of time needed to complete: 1 H
+
+Start time: 6:20
+
+Finish time: 7.45
+
+Actual time needed to complete: 1.25 H
