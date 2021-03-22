7ca746aa3da626f34443b26056b5b8ef2d520ecb
NihadZeidan <nihadzeidan@gmail.com>
2021-03-22 23:46:23 +0200

    not yet

--- JS/app.js
+++ JS/app.js
@@ -1,80 +1,79 @@
 'use strict';
-$('document').ready(function () {
-    
-
-function Horn(horn) {
-    this.title = horn.title;
-    this.description = horn.description;
-    this.image = horn.image_url;
-    this.keyword = horn.keyword;
-}
+$('document').ready(function() {
 
 
-Horn.prototype.renderFromClone = function () {
-    let template = $('#photo-template').clone();
-    template.find("h2").text(this.title);
-    template.find('p').text(this.description);
-    template.find('img').attr("src", this.image);
-    template.removeAttr('id')
-    $('main').append(template);
-
-}
+    function Horn(horn) {
+        this.title = horn.title;
+        this.description = horn.description;
+        this.image = horn.image_url;
+        this.keyword = horn.keyword;
+    }
 
 
-Horn.prototype.addToSelect = function () {
-    $('select').append(
-        `<option value="${this.keyword}"> ${this.keyword} </option>`
-    )
+    Horn.prototype.renderFromClone = function() {
+        let template = $('#photo-template').clone();
+        template.find("h2").text(this.title);
+        template.find('p').text(this.description);
+        template.find('img').attr("src", this.image);
+        template.removeAttr('id')
+        $('main').append(template);
 
-    // $('option').on("click", function () {
-    //     $("div").remove();
-    // })
-
-
-}
+    }
 
 
+    Horn.prototype.addToSelect = function() {
+        $('select').append(
+            `<option value="${this.keyword}"> ${this.keyword} </option>`
+        )
+
+        // $('option').on("click", function () {
+        //     $("div").remove();
+        // })
 
 
-
-let ajaxSettings = {
-    method: 'get',
-    dataType: 'json'
-}
-
-
-
-$.ajax('data/page-1.json', ajaxSettings).then((data) => {
-    data.forEach(function (horn, index) {
-        let newHorn = new Horn(horn);
-        newHorn.renderFromClone();
+    }
 
 
 
 
 
-        if (horn.keyword != $("option").attr('value')) {
-            newHorn.addToSelect();
-            console.log($("option").attr('value'));
-        } else {
-            $('option').hide();
-        }
+    let ajaxSettings = {
+        method: 'get',
+        dataType: 'json'
+    }
 
-        // console.log(keyWord);
+
+
+    $.ajax('data/page-1.json', ajaxSettings).then((data) => {
+        data.forEach(function(horn, index) {
+            let newHorn = new Horn(horn);
+            newHorn.renderFromClone();
+
+
+
+
+
+            if (horn.keyword != $("option").attr('value')) {
+                newHorn.addToSelect();
+                console.log($("option").attr('value'));
+            } else {
+                $('option').hide();
+            }
+
+            // console.log(keyWord);
+
+        })
+
+
+
+        // This to remove the defult template after render all the inctances:
+        $('#photo-template').remove()
+
+
+        // let keyWord = [];
+
 
     })
 
 
-
-    // This to remove the defult template after render all the inctances:
-    $('#photo-template').remove()
-
-
-    // let keyWord = [];
-
-
 })
-
-
-})
-
