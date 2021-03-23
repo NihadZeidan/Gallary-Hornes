b01dcb7c62f2e0cf68b1a812f452840ac0bfdb03
NihadZeidan <nihadzeidan@gmail.com>
2021-03-23 15:05:15 +0200

    almost finished

--- JS/app.js
+++ JS/app.js
@@ -2,89 +2,88 @@
 $('document').ready(function() {
 
 
-            function Horn(horn) {
-                this.title = horn.title;
-                this.description = horn.description;
-                this.image = horn.image_url;
-                this.keyword = horn.keyword;
-                this.horns = horn.horns;
-                Horn.all.push(this);
-            }
-
-            Horn.all = [];
-
-            Horn.prototype.renderFromClone = function() {
-                let template = $('#photo-template').clone();
-                template.find("h2").text(`${this.title}   #${this.horns}`);
-                template.find('p').text(this.description);
-                template.find('img').attr("src", this.image);
-                template.attr('class', `${this.keyword}  filtered`);
-                $('main').append(template);
-
-            }
+    let ajaxSettings = {
+        method: 'get',
+        dataType: 'json'
+    }
 
 
-            Horn.prototype.addToSelect = function() {
-                $('select').append(
-                    `<option value = "${this.keyword}"> ${this.keyword} </option>`
-                )
+    function Horn(horn) {
+        this.title = horn.title;
+        this.description = horn.description;
+        this.image = horn.image_url;
+        this.keyword = horn.keyword;
+        this.horns = horn.horns;
+        Horn.all.push(this);
+    }
+
+    Horn.all = [];
+
+    Horn.prototype.renderFromClone = function() {
+        let template = $('#photo-template').clone();
+        template.find("h2").text(`${this.title}   #${this.horns}`);
+        template.find('p').text(this.description);
+        template.find('img').attr("src", this.image);
+        template.attr('class', `${this.keyword}  filtered`);
+        $('main').append(template);
+
+    }
 
 
-            }
-
-
-
-
-            let ajaxSettings = {
-                method: 'get',
-                dataType: 'json'
-            }
-
-            $.ajax('data/page-1.json', ajaxSettings).then((data) => {
-                data.forEach(function(horn, index) {
-                    let newHorn = new Horn(horn);
-                    newHorn.renderFromClone();
-                    newHorn.addToSelect();
+    Horn.prototype.addToSelect = function() {
+        $('select').append(
+            `<option value = "${this.keyword}"> ${this.keyword} </option>`
+        )
+    }
 
 
 
+    $.ajax('data/page-1.json', ajaxSettings).then((data) => {
+        data.forEach(function(horn) {
+            let newHorn = new Horn(horn);
+            newHorn.renderFromClone();
+            newHorn.addToSelect();
+        })
 
 
-                    if (horn.keyword != $("option").attr('value')) {
-                        console.log($("option").attr('value'));
+
+        // This to remove the defult template after render all the inctances:
+        $('#photo-template').remove()
+
+
+    })
+
+
+
+    $('select').on("click", function(e) {
+        let selected = $(this).val();
+        console.log(selected);
+
+        if (selected !== "default") {
+
+            Horn.all.forEach(horn => {
+
+                    if (horn.keyword === selected) {
+
+                        // this meanes if the keyword is the same to the selected will add filtered istead of the class keyword and it will result in class = filtered filtered  (we identified the defalute filtered class as (display block))
+
+                        $(`.${selected}`).addClass('filtered');
+
                     } else {
-                        $('option').hide();
-                    }
 
-                })
+                        // if not matching will remove the filtered from the class it self and will keep the keyword it self (we identified the defalute div as a (display none) in CSS)
 
-
-
-                // This to remove the defult template after render all the inctances:
-                $('#photo-template').remove()
-
-
-            })
-
-
-
-            $('select').on("click", function(e) {
-                    let selected = $(this).val();
-                    console.log(selected);
-
-                    if (selected !== "default") {
-                        $("div").hide();
-
-                        Horn.all.forEach(horn => {
-                            if (horn.keyword === selected) {
-
-                                $(`.${horn.keyword}`).addClass($(this.keyword))
-                            } else {
-
-                                $(`.${selected}`).removeClass('filtered')
-                            }
-                        })
+                        $(`.${horn.keyword}`).removeClass('filtered');
 
                     }
-                )
-            }
+
+
+                }
+
+
+            )
+        }
+    })
+
+
+})
