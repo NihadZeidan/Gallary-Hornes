d5b3449d166acf2d446068766115e661be6590bd
NihadZeidan <nihadzeidan@gmail.com>
2021-03-23 10:42:43 +0200

    not finished yet

--- JS/app.js
+++ JS/app.js
@@ -2,78 +2,89 @@
 $('document').ready(function() {
 
 
-    function Horn(horn) {
-        this.title = horn.title;
-        this.description = horn.description;
-        this.image = horn.image_url;
-        this.keyword = horn.keyword;
-    }
-
-
-    Horn.prototype.renderFromClone = function() {
-        let template = $('#photo-template').clone();
-        template.find("h2").text(this.title);
-        template.find('p').text(this.description);
-        template.find('img').attr("src", this.image);
-        template.removeAttr('id')
-        $('main').append(template);
-
-    }
-
-
-    Horn.prototype.addToSelect = function() {
-        $('select').append(
-            `<option value="${this.keyword}"> ${this.keyword} </option>`
-        )
-
-        // $('option').on("click", function () {
-        //     $("div").remove();
-        // })
-
-
-    }
-
-
-
-
-
-    let ajaxSettings = {
-        method: 'get',
-        dataType: 'json'
-    }
-
-
-
-    $.ajax('data/page-1.json', ajaxSettings).then((data) => {
-        data.forEach(function(horn, index) {
-            let newHorn = new Horn(horn);
-            newHorn.renderFromClone();
-
-
-
-
-
-            if (horn.keyword != $("option").attr('value')) {
-                newHorn.addToSelect();
-                console.log($("option").attr('value'));
-            } else {
-                $('option').hide();
+            function Horn(horn) {
+                this.title = horn.title;
+                this.description = horn.description;
+                this.image = horn.image_url;
+                this.keyword = horn.keyword;
+                this.horns = horn.horns;
+                Horn.all.push(this);
             }
 
-            // console.log(keyWord);
+            Horn.all = [];
 
-        })
+            Horn.prototype.renderFromClone = function() {
+                let template = $('#photo-template').clone();
+                template.find("h2").text(`${this.title}   #${this.horns}`);
+                template.find('p').text(this.description);
+                template.find('img').attr("src", this.image);
+                template.attr('class', `${this.keyword}  filtered`);
+                $('main').append(template);
 
-
-
-        // This to remove the defult template after render all the inctances:
-        $('#photo-template').remove()
-
-
-        // let keyWord = [];
-
-
-    })
+            }
 
 
-})
+            Horn.prototype.addToSelect = function() {
+                $('select').append(
+                    `<option value = "${this.keyword}"> ${this.keyword} </option>`
+                )
+
+
+            }
+
+
+
+
+            let ajaxSettings = {
+                method: 'get',
+                dataType: 'json'
+            }
+
+            $.ajax('data/page-1.json', ajaxSettings).then((data) => {
+                data.forEach(function(horn, index) {
+                    let newHorn = new Horn(horn);
+                    newHorn.renderFromClone();
+                    newHorn.addToSelect();
+
+
+
+
+
+                    if (horn.keyword != $("option").attr('value')) {
+                        console.log($("option").attr('value'));
+                    } else {
+                        $('option').hide();
+                    }
+
+                })
+
+
+
+                // This to remove the defult template after render all the inctances:
+                $('#photo-template').remove()
+
+
+            })
+
+
+
+            $('select').on("click", function(e) {
+                    let selected = $(this).val();
+                    console.log(selected);
+
+                    if (selected !== "default") {
+                        $("div").hide();
+
+                        Horn.all.forEach(horn => {
+                            if (horn.keyword === selected) {
+
+                                $(`.${horn.keyword}`).addClass($(this.keyword))
+                            } else {
+
+                                $(`.${selected}`).removeClass('filtered')
+                            }
+                        })
+
+                    }
+                )
+            }
