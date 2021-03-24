b0ea57f604b724c9731b7fdb25238cf04da73bd3
NihadZeidan <nihadzeidan@gmail.com>
2021-03-24 15:03:54 +0200

    FINISHED

--- JS/app.js
+++ JS/app.js
@@ -1,137 +1,87 @@
-'use strict';
-$('document').ready(function() {
+  'use strict';
+  $('document').ready(function() {
 
+      let ajaxSettings = {
+          method: 'get',
+          dataType: 'json'
+      }
 
-    let ajaxSettings = {
-        method: 'get',
-        dataType: 'json'
-    }
+      let arrayForKey = [];
 
-    let arrayForKey = [];
+      HornForPageOne.all = [];
 
-    function HornForPageOne(horn) {
-        this.title = horn.title;
-        this.description = horn.description;
-        this.image_url = horn.image_url;
-        this.keyword = horn.keyword;
-        this.horns = horn.horns;
-        HornForPageOne.all.push(this);
-        arrayForKey.push(horn.keyword);
-    }
 
-    HornForPageOne.all = [];
+      function HornForPageOne(horn) {
+          this.title = horn.title;
+          this.description = horn.description;
+          this.image_url = horn.image_url;
+          this.keyword = horn.keyword;
+          this.horns = horn.horns;
+          HornForPageOne.all.push(this);
+      }
 
+      HornForPageOne.all = [];
 
-    HornForPageOne.prototype.toRender = function() {
-        let templateOne = $('#photo-template').html();
-        let html = Mustache.render(templateOne, this);
-        $('main').append(html);
 
-        let createOption = $('<option value="default"> </option>').attr('value', this.keyword).text(this.keyword)
-        $('select').append(createOption)
+      HornForPageOne.prototype.toRender = function() {
+          let template = $('#photo-template').clone();
+          template.find("h2").text(`${this.title}   #${this.horns}`);
+          template.find('p').text(this.description);
+          template.find('img').attr("src", this.image_url);
+          template.attr('class', `${this.keyword}  filtered`);
+          $('main').append(template);
+      }
 
-    }
 
 
 
-    function renderFirst() {
 
-        $.ajax('data/page-1.json', ajaxSettings).then((data) => {
+      $.ajax('data/page-1.json', ajaxSettings).then((data) => {
+          data.forEach((horn, i) => {
 
+              let newHorn = new HornForPageOne(horn);
+              newHorn.toRender();
 
-            data.forEach(function(horn, i) {
+              if (!arrayForKey.includes(HornForPageOne.all[i].keyword)) {
+                  arrayForKey.push(HornForPageOne.all[i].keyword)
+              }
+          })
 
+          for (let i = 0; i < arrayForKey.length; i++) {
+              $('select').append(
+                  `<option value = "${arrayForKey[i]}"> ${arrayForKey[i]} </option>`
+              )
+          }
+          HornForPageOne.all = [];
+      })
 
-                let newHorn = new HornForPageOne(horn);
 
-                newHorn.toRender();
+      $('select').on("click", function(e) {
+          let selected = $(this).val();
+          console.log(selected);
 
+          if (selected !== "default") {
 
 
-                // if (arrayForKey[i] !== $('option').val()) {
-                //     $('select').append(
-                //         `<option value = "${arrayForKey[i]}"> ${arrayForKey[i]} </option>`
-                //     )
+              $.ajax('data/page-1.json', ajaxSettings).then((data) => {
+                  data.forEach((horn, i) => {
+                      if (horn.keyword === selected) {
 
-                // }
+                          // this meanes if the keyword is the same to the selected will add filtered istead of the class keyword and it will result in class = filtered filtered  (we identified the defalute filtered class as (display block))
 
+                          $(`.${selected}`).addClass('filtered');
 
-            })
+                      } else {
 
+                          // if not matching will remove the filtered from the class it self and will keep the keyword it self (we identified the defalute div as a (display none) in CSS)
 
+                          $(`.${horn.keyword}`).removeClass('filtered');
 
+                      }
 
+                  })
 
-        })
-
-    }
-
-    renderFirst();
-
-
-    //  To render the data from first JSON
-    $("#first").on("click", function() {
-        HornForPageOne.all = [];
-        $('select').empty();
-        renderFirst();
-
-    })
-
-
-
-    //  To render the data from second JSON
-    $("#second").on("click", function() {
-        HornForPageOne.all = [];
-        $('select').empty();
-        console.log(HornForPageOne.all);
-
-        $.ajax('data/page-2.json', ajaxSettings).then((data) => {
-
-            data.forEach(function(horn) {
-
-                let newHorn = new HornForPageOne(horn);
-                newHorn.toRender();
-
-
-                $('select').append(
-                    `<option value = "${horn.keyword}"> ${horn.keyword} </option>`
-                )
-
-
-            })
-
-        })
-    })
-
-})
-
-
-
-// $('select').on("click", function(e) {
-//     let selected = $(this).val();
-//     console.log(selected);
-
-//     if (selected !== "default") {
-
-
-//         if (horn.keyword === selected) {
-
-//             // this meanes if the keyword is the same to the selected will add filtered istead of the class keyword and it will result in class = filtered filtered  (we identified the defalute filtered class as (display block))
-
-//             $(`.${selected}`).addClass('filtered');
-
-//         } else {
-
-//             // if not matching will remove the filtered from the class it self and will keep the keyword it self (we identified the defalute div as a (display none) in CSS)
-
-//             $(`.${horn.keyword}`).removeClass('filtered');
-
-//         }
-
-
-
-
-
-
-//     }
-// })
+              })
+          }
+      })
+  })
