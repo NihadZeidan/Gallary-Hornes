3abdd8bb91a5384fe877126a6c25ca3c2f01bb80
NihadZeidan <nihadzeidan@gmail.com>
2021-03-24 14:39:43 +0200

    not finished

--- JS/app.js
+++ JS/app.js
@@ -1,136 +1,147 @@
 'use strict';
 $('document').ready(function() {
 
-            let ajaxSettings = {
-                method: 'get',
-                dataType: 'json'
-            }
+    let ajaxSettings = {
+        method: 'get',
+        dataType: 'json'
+    }
 
-            let arrayForKey = [];
-            Horns.all = [];
+    let arrayForKey = [];
+    Horns.all = [];
 
-            function Horns(horn) {
-                this.title = horn.title;
-                this.description = horn.description;
-                this.image_url = horn.image_url;
-                this.keyword = horn.keyword;
-                this.horns = horn.horns;
-                Horns.all.push(this);
-            }
+    function Horns(horn) {
+        this.title = horn.title;
+        this.description = horn.description;
+        this.image_url = horn.image_url;
+        this.keyword = horn.keyword;
+        this.horns = horn.horns;
+        Horns.all.push(this);
+    }
 
 
 
-            HornForPageOne.prototype.toRender = function() {
+    // Horns.prototype.toRender = function() {
+
+    // }
+
+
+
+
+
+    function renderAjax(num) {
+        arrayForKey = [];
+        Horns.all = [];
+        $('option').remove();
+        $('section').remove();
+
+
+        $.ajax(`data/page-${num}.json`, ajaxSettings).then((data) => {
+            data.forEach((horn, i) => {
+                let newHorn = new Horns(horn);
+
                 let templateOne = $('#photo-template').html();
-                let html = Mustache.render(templateOne, this);
+                let html = Mustache.render(templateOne, newHorn);
                 $('main').append(html);
-            }
+
+                if (!arrayForKey.includes(Horns.all[i].keyword)) {
+                    arrayForKey.push(Horns.all[i].keyword)
+                    $('select').append(
+                        `<option value = "${horn.keyword}"> ${horn.keyword} </option>`
+                    )
+                }
+            })
+
+        })
+    }
 
 
 
 
+    function clickOnPage() {
+        $('button').on("click", function() {
+            let id = $(this).attr('id');
+            renderAjax(id);
+        })
+    }
 
-            function renderAjax(num) {
-                arrayForKey = [];
-                Horns.all = [];
+    clickOnPage();
 
-
-                $.ajax(`data/page-${num}.json`, ajaxSettings).then((data) => {
-                    data.forEach((horn, i) => {
-                        let newHorn = new Horns(horn);
-                        newHorn.toRender();
-
-                        if (!arrayForKey.includes(Horns.all[i].keyword)) {
-                            arrayForKey.push(Horns.all[i].keyword)
-                            $('select').append(
-                                `<option value = "${arrayForKey[i]}"> ${arrayForKey[i]} </option>`
-                            )
-                        }
-                    })
-
-                    // for (let i = 0; i < arrayForKey.length; i++) {}
-
-                })
-            }
-
-            function clickOnPage() {
-                let numberOfPAge = $('button')
-                numberOfPAge.on("click", () => {
-                    $(this).attr('id')
-                    renderAjax(numberOfPAge);
-                })
+    // renderAjax(1);
 
 
 
 
-                //  To render the data from first JSON
-                // $("#first").on("click", function() {
-                //     HornForPageOne.all = [];
-                //     $('select').empty();
-                //     renderFirst();
-
-                // })
+})
 
 
+//  To render the data from first JSON
+// $("#first").on("click", function() {
+//     HornForPageOne.all = [];
+//     $('select').empty();
+//     renderFirst();
 
-                //  To render the data from second JSON
-                //     $("#second").on("click", function(e) {
-                //         HornForPageOne.all = [];
-                //         console.log(HornForPageOne.all);
-                //         $('select').empty();
-                //         arrayForKey = [];
-
-                //         $.ajax('data/page-2.json', ajaxSettings).then((data) => {
-                //             data.forEach(function(horn, i) {
-
-                //                 let newHorn = new HornForPageOne(horn);
-                //                 newHorn.toRender();
-
-                //                 if (!arrayForKey.includes(HornForPageOne.all[i].keyword)) {
-                //                     arrayForKey.push(HornForPageOne.all[i].keyword)
-                //                 }
-                //             })
-
-
-                //             for (let i = 0; i < arrayForKey.length; i++) {
-                //                 $('select').append(
-                //                     `<option value = "${arrayForKey[i]}"> ${arrayForKey[i]} </option>`
-                //                 )
-                //             }
-
-                //         })
-
-                //     })
-
-                // })
+// })
 
 
 
-                // $('select').on("click", function(e) {
-                //     let selected = $(this).val();
-                //     console.log(selected);
+//  To render the data from second JSON
+//     $("#second").on("click", function(e) {
+//         HornForPageOne.all = [];
+//         console.log(HornForPageOne.all);
+//         $('select').empty();
+//         arrayForKey = [];
 
-                //     if (selected !== "default") {
+//         $.ajax('data/page-2.json', ajaxSettings).then((data) => {
+//             data.forEach(function(horn, i) {
+
+//                 let newHorn = new HornForPageOne(horn);
+//                 newHorn.toRender();
+
+//                 if (!arrayForKey.includes(HornForPageOne.all[i].keyword)) {
+//                     arrayForKey.push(HornForPageOne.all[i].keyword)
+//                 }
+//             })
 
 
-                //         if (horn.keyword === selected) {
+//             for (let i = 0; i < arrayForKey.length; i++) {
+//                 $('select').append(
+//                     `<option value = "${arrayForKey[i]}"> ${arrayForKey[i]} </option>`
+//                 )
+//             }
 
-                //             // this meanes if the keyword is the same to the selected will add filtered istead of the class keyword and it will result in class = filtered filtered  (we identified the defalute filtered class as (display block))
+//         })
 
-                //             $(`.${selected}`).addClass('filtered');
+//     })
 
-                //         } else {
+// })
 
-                //             // if not matching will remove the filtered from the class it self and will keep the keyword it self (we identified the defalute div as a (display none) in CSS)
 
-                //             $(`.${horn.keyword}`).removeClass('filtered');
 
-                //         }
+// $('select').on("click", function(e) {
+//     let selected = $(this).val();
+//     console.log(selected);
+
+//     if (selected !== "default") {
+
+
+//         if (horn.keyword === selected) {
+
+//             // this meanes if the keyword is the same to the selected will add filtered istead of the class keyword and it will result in class = filtered filtered  (we identified the defalute filtered class as (display block))
+
+//             $(`.${selected}`).addClass('filtered');
+
+//         } else {
+
+//             // if not matching will remove the filtered from the class it self and will keep the keyword it self (we identified the defalute div as a (display none) in CSS)
+
+//             $(`.${horn.keyword}`).removeClass('filtered');
+
+//         }
 
 
 
 
 
 
-                //     }
-                // })
+//     }
+// })
